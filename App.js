12a23a6bd6c3e8ae7bf02dedb733335c50943c96
munakalamadhuri <munakalamadhuri405@gmail.com>
2023-11-29 16:19:59 +0530

    added new elements

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 var jsxHeading = (
     <div>
-<h1 id="jsxHeading">Heading from JSX!!</h1>;
-<h1 id="jsxHeading2">Heading from JSX!!</h1>;
+<h1 id="jsxHeading">Heading from JSX!!</h1>
+<h1 id="jsxHeading2">Heading from JSX!!</h1>
 <h1>Hello, world!</h1>
       <p>This is a paragraph.</p>
       <ul>
@@ -12,10 +12,14 @@ var jsxHeading = (
         <li>Item 2</li>
         <li>Item 3</li>
       </ul>
+      <a href="https://www.w3schools.com">Visit W3Schools.com!</a>
+      <br/>
+      <br/>
+      <button>click me</button>
     </div>
    
 );
+var htmlRoot = document.getElementById("root");
+var rootReact = ReactDOM.createRoot(htmlRoot);
+rootReact.render(jsxHeading);
 
-
-// Use ReactDOM.render instead of rootReact.render
-ReactDOM.render(jsxHeading, document.getElementById('root'));
