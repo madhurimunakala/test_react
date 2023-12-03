77cbf38dc0bc3d040eb4d5289f9b4c7ba6571854
munakalamadhuri <munakalamadhuri405@gmail.com>
2023-12-03 09:41:55 +0530

    updated the App.js` and package.json

--- App.js
+++ App.js
@@ -19,7 +19,40 @@ var jsxHeading = (
     </div>
    
 );
-var htmlRoot = document.getElementById("root");
-var rootReact = ReactDOM.createRoot(htmlRoot);
-rootReact.render(jsxHeading);
+const nestedheader=
+    React.createElement('div', { className: 'title' },
+    [
+    React.createElement('h1', {}, 'Heading 1 using react'),
+    React.createElement('h2', {}, 'Heading 2 using react'),
+    React.createElement('h3', {}, 'Heading 3 using react'),
+    ]
+  )
+  const nestedHeader =(
+      <div className="title">
+        <h1>Heading 1 using JSX</h1>
+        <h2>Heading 2 using JSX</h2>
+        <h3>Heading 3 using JSX</h3>
+      </div>
+    );
+    const InsideComponent = function(){
+      return <h5 style={{ color: 'pink', fontSize: '12px' }}> Heading 5</h5>
+  }
+  
+    const NestedHeader = () => {
+      return (
+        <div className="title">
+          {jsxHeading}
+          <h1 style={{ color: 'blue', fontSize: '24px' }}>Heading 1</h1>
+          <InsideComponent/>
+          <h2 style={{ color: 'red', fontSize: '20px' }}>Heading 2</h2>
+          <h3 style={{ color: 'green', fontSize: '16px' }}>Heading 3</h3>
+          <InsideComponent> </InsideComponent>
+        </div>
+      );
+    };
+    
+
+const htmlRoot = document.getElementById("root");
+const rootReact = ReactDOM.createRoot(htmlRoot);
+rootReact.render(<NestedHeader/>);
 
